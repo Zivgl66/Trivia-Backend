9c90af3447bd81545dfd80bb58652764563fb158
Zivgl66 <95753868+Zivgl66@users.noreply.github.com>
2023-10-01 19:49:06 +0300

    updated api routes

--- routes/api/api.js
+++ routes/api/api.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const usersRouter = require("./users");
 const roomsRouter = require("./rooms");
 const guestsRouter = require("./guest");
+const gamesRouter = require("./game");
 
 //http://localhost:3001/api/users
 router.use("/users", usersRouter);
@@ -12,5 +13,8 @@ router.use("/guests", guestsRouter);
 
 //http://localhost:3001/api/rooms
 router.use("/rooms", roomsRouter);
+
+//http://localhost:3001/api/rooms
+router.use("/games", gamesRouter);
 
 module.exports = router;
