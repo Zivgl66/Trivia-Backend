702d7b9e4bc047ed32e63b2bbb4c545f9789cdf5
Zivgl66 <95753868+Zivgl66@users.noreply.github.com>
2023-10-02 17:29:45 +0300

    Added 2 dynamic id routers

--- routes/api/playerResult.js
+++ routes/api/playerResult.js
@@ -36,3 +36,40 @@ router.post("/", async (req, res) => {
     res.json(err).status(401);
   }
 });
+
+//--GET-- get a player result by id
+router.get("/:id", async (req, res) => {
+  try {
+    const playerResult = await playerResultsModule.getPlayerResultById(
+      req.params.id
+    );
+    if (!playerResult)
+      res.json({ message: "player result not found" }).status(401);
+    else
+      res.json({
+        status: "success",
+        message: "Player results Found",
+        playerResult,
+      });
+  } catch (err) {
+    console.error("error getting the players results: ", err);
+    res.json(err).status(401);
+  }
+});
+
+//--POST-- Delete a players result
+router.post("/:id", async (req, res) => {
+  try {
+    let deleted = await playerResultsModule.deletePlayersResults(req.params.id);
+    if (deleted)
+      res.json({
+        status: "success",
+        message: "Player result Deleted",
+        playerResult: deleted,
+      });
+    else res.json({ message: "player result not found!" }).status(304);
+  } catch (err) {
+    console.error("error deleting the players results: ", err);
+    res.json(err).status(401);
+  }
+});
